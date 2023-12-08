1b4b6ebf438089cdcc1b64c4788e31e4e5640fb6
redstonekasi <kasi@hanneskann.de>
2023-12-08 02:53:33 +0100

    fix experiments

--- packages/core-extensions/src/experiments/index.ts
+++ packages/core-extensions/src/experiments/index.ts
@@ -2,27 +2,10 @@ import { Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
   {
-    find: /\.displayName="(Developer)?ExperimentStore"/,
+    find: "isStaffEnv:",
     replace: {
-      match: "window.GLOBAL_ENV.RELEASE_CHANNEL",
-      replacement: '"staging"'
+      match: /.\.isStaff\(\)/,
+      replacement: "!0"
     }
-  },
-  {
-    find: '.displayName="DeveloperExperimentStore"',
-    replace: [
-      {
-        match: /CONNECTION_OPEN:.,OVERLAY_INITIALIZE:.,CURRENT_USER_UPDATE:./,
-        replacement: ""
-      },
-      {
-        match: '"production"',
-        replacement: '"development"'
-      },
-      {
-        match: /(get:function\(\){return .}}}\);).\(\);/,
-        replacement: "$1"
-      }
-    ]
   }
 ];
