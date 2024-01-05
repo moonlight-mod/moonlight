46757cd405c5294652587ab0e4ed3ca92d15c402
NotNite <hi@notnite.com>
2024-01-05 12:22:02 -0500

    Rework overlay

--- flake.nix
+++ flake.nix
@@ -7,19 +7,14 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+    let
+      version = "1.0.7";
+      hash = "sha256-jqy9RM2/9i/o4TTzKG1FvnoUCjwslBaHVG2tIx/T2f4=";
 
-        version = "1.0.7";
-        hash = "sha256-jqy9RM2/9i/o4TTzKG1FvnoUCjwslBaHVG2tIx/T2f4=";
-
-        # pnpm2nix is unmaintained, forks of it don't support pnpm workspaces
-        # Downloading tarballs from GitHub releases will have to do
-        moonlight = pkgs.stdenv.mkDerivation {
+      # pnpm2nix is unmaintained, forks of it don't support pnpm workspaces
+      # Downloading tarballs from GitHub releases will have to do
+      mkMoonlight = { pkgs }:
+        pkgs.stdenv.mkDerivation {
           name = "moonlight";
           version = version;
           src = pkgs.fetchurl {
@@ -42,54 +37,61 @@
           };
         };
 
-        mkOverride = discord: name:
-          discord.overrideAttrs (old: {
-            installPhase = let
-              origAsar = "$out/opt/${name}/resources/app.asar";
-              movedAsar = "$out/opt/${name}/resources/_app.asar";
-              app = "$out/opt/${name}/resources/app";
+      mkOverride = discord: name: moonlight:
+        discord.overrideAttrs (old: {
+          installPhase = let
+            origAsar = "$out/opt/${name}/resources/app.asar";
+            movedAsar = "$out/opt/${name}/resources/_app.asar";
+            app = "$out/opt/${name}/resources/app";
 
-              injected = ''
-                require("${moonlight}/injector").inject(
-                  require("path").join(__dirname, "../_app.asar")
-                );
-              '';
-
-              packageJson = ''
-                {"name":"discord","main":"./injector.js","private":true}
-              '';
-
-            in old.installPhase + "\n" + ''
-              mv ${origAsar} ${movedAsar}
-              mkdir -p ${app}
-
-              cat > ${app}/injector.js <<EOF
-              ${injected}
-              EOF
-
-              echo '${packageJson}' > ${app}/package.json
+            injected = ''
+              require("${moonlight}/injector").inject(
+                require("path").join(__dirname, "../_app.asar")
+              );
             '';
-          });
 
-        overlay = final: prev: {
-          discord = mkOverride prev.discord "Discord";
-          discord-ptb = mkOverride prev.discord-ptb "DiscordPTB";
-          discord-canary = mkOverride prev.discord-canary "DiscordCanary";
-          discord-development =
-            mkOverride prev.discord-development "DiscordDevelopment";
-        };
+            packageJson = ''
+              {"name":"discord","main":"./injector.js","private":true}
+            '';
 
-        pkgsWithOverlay = import nixpkgs {
+          in old.installPhase + "\n" + ''
+            mv ${origAsar} ${movedAsar}
+            mkdir -p ${app}
+
+            cat > ${app}/injector.js <<EOF
+            ${injected}
+            EOF
+
+            echo '${packageJson}' > ${app}/package.json
+          '';
+        });
+
+      overlay = final: prev: rec {
+        moonlight-mod = mkMoonlight { pkgs = final; };
+        discord = mkOverride prev.discord "Discord" moonlight-mod;
+        discord-ptb = mkOverride prev.discord-ptb "DiscordPTB" moonlight-mod;
+        discord-canary =
+          mkOverride prev.discord-canary "DiscordCanary" moonlight-mod;
+        discord-development =
+          mkOverride prev.discord-development "DiscordDevelopment"
+          moonlight-mod;
+      };
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
           overlays = [ overlay ];
         };
       in {
-        packages.default = moonlight;
+        packages.default = pkgs.moonlight-mod;
+        packages.moonlight-mod = pkgs.moonlight-mod;
+
+        packages.discord = pkgs.discord;
+        packages.discord-ptb = pkgs.discord-ptb;
+        packages.discord-canary = pkgs.discord-canary;
+        packages.discord-development = pkgs.discord-development;
+      }) // {
         overlays.default = overlay;
-        packages.discord = pkgsWithOverlay.discord;
-        packages.discord-ptb = pkgsWithOverlay.discord-ptb;
-        packages.discord-canary = pkgsWithOverlay.discord-canary;
-        packages.discord-development = pkgsWithOverlay.discord-development;
-      });
+      };
 }
