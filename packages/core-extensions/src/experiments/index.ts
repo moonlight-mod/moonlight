5d1349514a6c50a576be1149786647125dbc2855
redstonekasi <kasi@hanneskann.de>
2024-02-08 21:37:19 +0100

    old experiments patches v2

--- packages/core-extensions/src/experiments/index.ts
+++ packages/core-extensions/src/experiments/index.ts
@@ -2,45 +2,17 @@ import { Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
   {
-    find: '.displayName="ExperimentStore"',
+    find: "isStaffEnv:",
     replace: {
-      match: "window.GLOBAL_ENV.RELEASE_CHANNEL",
-      replacement: '"staging"'
+      match: /.\.isStaff\(\)/,
+      replacement: "!0"
     }
   },
   {
-    find: '.displayName="DeveloperExperimentStore"',
-    replace: [
-      {
-        match: /CONNECTION_OPEN:.,OVERLAY_INITIALIZE:.,CURRENT_USER_UPDATE:./,
-        replacement: ""
-      },
-      {
-        match: '"production"',
-        replacement: '"development"'
-      },
-      {
-        match: /get:\(\)=>./,
-        replacement: "get:()=>true"
-      }
-    ]
-  },
-
-  {
-    find: ".HEADER_BAR)",
+    find: '"scientist:triggered"', // Scientist? Triggered.
     replace: {
-      match:
-        /,(.)\?(\(0,.\.jsx\)\(.{1,3}\.default,{}\)):(\(0,.\.jsx\)\(.{1,3}\.default,{}\))\]/,
-      replacement: (_, isStaff, StaffHelpButton, HelpButton) =>
-        `,(moonlight.getConfigOption("experiments","staffSettings")??${isStaff})?${StaffHelpButton}:${HelpButton}]`
-    }
-  },
-  {
-    find: 'title:"Developer Flags"',
-    replace: {
-      match: /\(null==.\?void 0:.\.isStaff\(\)\)/g,
-      replacement: (orig: string) =>
-        `(moonlight.getConfigOption("experiments","staffSettings")?true:${orig})`
+      match: /(?<=personal_connection_id\|\|)!1/,
+      replacement: "!0"
     }
   }
 ];
