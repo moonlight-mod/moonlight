5871f1f452b284cf013248d9fb89ba5f3eed4eb0
redstonekasi <kasi@hanneskann.de>
2024-06-19 21:03:37 +0200

    moonbase: remove danger icon svg

--- packages/core-extensions/src/moonbase/types.ts
+++ packages/core-extensions/src/moonbase/types.ts
@@ -1,8 +1,5 @@
 import { DetectedExtension, ExtensionManifest } from "types/src";
 
-export const DangerIconSVG =
-  "M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22Zm1.44-15.94L13.06 14a1.06 1.06 0 0 1-2.12 0l-.38-6.94a1 1 0 0 1 1-1.06h.88a1 1 0 0 1 1 1.06Zm-.19 10.69a1.25 1.25 0 1 1-2.5 0 1.25 1.25 0 0 1 2.5 0Z";
-
 export type MoonbaseNatives = {
   fetchRepositories(
     repos: string[]
