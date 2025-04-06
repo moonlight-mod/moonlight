3faec24277a1b08c8e002a84301b5fb5a5b985db
NotNite <hi@notnite.com>
2025-04-06 15:02:09 -0400

    moonbase/about: Cleanup
    Makes the dev component use a button instead of a card which looks nice.
    Also fixes the styling since it was a bit spaghetti.

--- packages/core-extensions/src/moonbase/webpackModules/ui/about.tsx
+++ packages/core-extensions/src/moonbase/webpackModules/ui/about.tsx
@@ -1,5 +1,4 @@
 import {
-  Card,
   Text,
   useThemeContext,
   Button,
@@ -10,7 +9,6 @@ import {
 import Flex from "@moonlight-mod/wp/discord/uikit/Flex";
 import React from "@moonlight-mod/wp/react";
 import MarkupUtils from "@moonlight-mod/wp/discord/modules/markup/MarkupUtils";
-import AppCardClasses from "@moonlight-mod/wp/discord/modules/guild_settings/web/AppCard.css";
 import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
 
 const wordmark = "https://raw.githubusercontent.com/moonlight-mod/moonlight/refs/heads/main/img/wordmark.png";
@@ -27,19 +25,13 @@ function parse(str: string) {
 
 function Dev({ name, picture, link }: { name: string; picture: string; link: string }) {
   return (
-    <Card editable={true} className={AppCardClasses.card}>
-      <div className={AppCardClasses.cardHeader + " moonbase-dev"}>
-        <Flex direction={Flex.Direction.HORIZONTAL} align={Flex.Align.CENTER}>
-          <img src={picture} alt={name} className="moonbase-dev-avatar" />
+    <Button onClick={() => window.open(link)} color={Button.Colors.PRIMARY} className="moonbase-dev">
+      <Flex direction={Flex.Direction.HORIZONTAL} align={Flex.Align.CENTER} className="moonbase-gap">
+        <img src={picture} alt={name} className="moonbase-dev-avatar" />
 
-          <Flex direction={Flex.Direction.VERTICAL} align={Flex.Align.CENTER}>
-            <a href={link} rel="noreferrer noopener" target="_blank" tabIndex={-1}>
-              <Text variant="text-md/semibold">{name}</Text>
-            </a>
-          </Flex>
-        </Flex>
-      </div>
-    </Card>
+        <Text variant="text-md/semibold">{name}</Text>
+      </Flex>
+    </Button>
   );
 }
 
@@ -85,34 +77,24 @@ export default function AboutPage() {
   const darkTheme = useThemeContext()?.theme !== "light";
 
   return (
-    <div>
-      <Flex direction={Flex.Direction.VERTICAL} align={Flex.Align.CENTER}>
-        <img src={darkTheme ? wordmarkLight : wordmark} alt="moonlight wordmark" className="moonbase-wordmark" />
-        <Text variant="heading-lg/medium">created by:</Text>
-        <div className="moonbase-devs">
-          <Dev name="Cynosphere" picture="https://github.com/Cynosphere.png" link="https://github.com/Cynosphere" />
-          <Dev name="NotNite" picture="https://github.com/NotNite.png" link="https://github.com/NotNite" />
-          <Dev name="adryd" picture="https://github.com/adryd325.png" link="https://github.com/adryd325" />
-          <Dev
-            name="redstonekasi"
-            picture="https://github.com/redstonekasi.png"
-            link="https://github.com/redstonekasi"
-          />
-        </div>
+    <Flex direction={Flex.Direction.VERTICAL} align={Flex.Align.CENTER} className="moonbase-about-page">
+      <img src={darkTheme ? wordmarkLight : wordmark} alt="moonlight wordmark" className="moonbase-wordmark" />
 
-        <Flex direction={Flex.Direction.HORIZONTAL} align={Flex.Align.CENTER} className="moonbase-gap">
-          <IconButton text="View source" icon={AngleBracketsIcon} link="https://github.com/moonlight-mod/moonlight" />
-          <IconButton text="Open the docs" icon={BookCheckIcon} link="https://moonlight-mod.github.io/" />
-          <IconButton
-            text="Join the server"
-            icon={ClydeIcon}
-            link="https://discord.gg/FdZBTFCP6F"
-            openInClient={true}
-          />
-        </Flex>
+      <Text variant="heading-lg/medium">created by:</Text>
+      <div className="moonbase-devs">
+        <Dev name="Cynosphere" picture="https://github.com/Cynosphere.png" link="https://github.com/Cynosphere" />
+        <Dev name="NotNite" picture="https://github.com/NotNite.png" link="https://github.com/NotNite" />
+        <Dev name="adryd" picture="https://github.com/adryd325.png" link="https://github.com/adryd325" />
+        <Dev name="redstonekasi" picture="https://github.com/redstonekasi.png" link="https://github.com/redstonekasi" />
+      </div>
+
+      <Flex direction={Flex.Direction.HORIZONTAL} align={Flex.Align.CENTER} className="moonbase-gap">
+        <IconButton text="View source" icon={AngleBracketsIcon} link="https://github.com/moonlight-mod/moonlight" />
+        <IconButton text="Open the docs" icon={BookCheckIcon} link="https://moonlight-mod.github.io/" />
+        <IconButton text="Join the server" icon={ClydeIcon} link="https://discord.gg/FdZBTFCP6F" openInClient={true} />
       </Flex>
 
-      <Flex direction={Flex.Direction.VERTICAL} align={Flex.Align.START} className="moonbase-about-text">
+      <Flex direction={Flex.Direction.VERTICAL} align={Flex.Align.START}>
         <Text variant="text-sm/normal">
           {parse(`moonlight \`${window.moonlight.version}\` on \`${window.moonlight.branch}\``)}
         </Text>
@@ -123,6 +105,6 @@ export default function AboutPage() {
           )}
         </Text>
       </Flex>
-    </div>
+    </Flex>
   );
 }
