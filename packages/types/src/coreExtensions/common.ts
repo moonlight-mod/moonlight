0fe2201252479a29fcc6f712b5de9611227cb79f
NotNite <hi@notnite.com>
2025-04-14 15:28:32 -0400

    Add common_icons

--- packages/types/src/coreExtensions/common.ts
+++ packages/types/src/coreExtensions/common.ts
@@ -1,3 +1,5 @@
+import type { IconProps, IconSize } from "@moonlight-mod/mappings/discord/components/common/index";
+
 export type ErrorBoundaryProps = React.PropsWithChildren<{
   noop?: boolean;
   fallback?: React.FC<any>;
@@ -9,3 +11,23 @@ export type ErrorBoundaryState = {
   error?: Error;
   componentStack?: string;
 };
+
+export type ErrorBoundary = React.ComponentClass<ErrorBoundaryProps, ErrorBoundaryState>;
+
+export type ParsedIconProps = {
+  width: number;
+  height: number;
+  fill: string;
+  className: string;
+};
+
+export interface Icons {
+  /**
+   * Parse icon props into their actual width/height.
+   * @param props The icon props
+   */
+  parseProps(props?: IconProps): ParsedIconProps;
+}
+
+// Re-export so extension developers don't need to depend on mappings
+export type { IconProps, IconSize };
