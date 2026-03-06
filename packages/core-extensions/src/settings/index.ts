79a25fa4f4336fee06229d9662d356ae34e52bae
Cynthia Foxwell <gamers@riseup.net>
2026-03-05 23:19:31 -0700

    settings: fix patch find, remove old settings patches as they are irrelevant

--- packages/core-extensions/src/settings/index.ts
+++ packages/core-extensions/src/settings/index.ts
@@ -2,33 +2,7 @@ import type { ExtensionWebExports, Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
   {
-    find: '{header:"Developer Only",',
-    replace: {
-      match: /(?<=\.push\(.+?\)}\)\)}\),)(.+?)}/,
-      replacement: (_, sections: string) => `require("settings_settings").default._mutateSections(${sections})}`
-    }
-  },
-  {
-    find: 'navId:"user-settings-cog",',
-    replace: [
-      {
-        match: /(?<=children:\[(\i)\.map\(.+?children:\((\i)=>{switch\(\i\){.+?)default:return null/,
-        replacement: (_, sections, section) =>
-          `default:return ${sections}.find(x=>x.section==${section})?._moonlight_submenu?.()`
-      },
-
-      // redesign
-      {
-        match: /=(Object\.values\(\i\.\i\))\.filter/,
-        replacement: (_, sections) =>
-          `=[...${sections},...Object.keys(require("settings_redesign").default.aliases)].filter`
-      }
-    ]
-  },
-
-  // redesign
-  {
-    find: ")?.parentAccordionKey",
+    find: ")?.parentPanelKey",
     replace: {
       match: /({node:\i,visibleDirectory:\i,accessibleDirectory:\i}=\(0,\i\.\i\)\()(\i),(\i\?\?"")/,
       replacement: (_, orig, sections, query) =>
