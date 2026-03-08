997dfefdf01c8a74537d9ae25afc6a3c2e9a72ec
Cynthia Foxwell <gamers@riseup.net>
2026-03-07 21:57:47 -0700

    noHideToken: add extra patch to stop callbacks

--- packages/core-extensions/src/noHideToken/index.ts
+++ packages/core-extensions/src/noHideToken/index.ts
@@ -7,5 +7,15 @@ export const patches: Patch[] = [
       match: /hideToken:\(\)=>.+?,/,
       replacement: `hideToken:()=>{},`
     }
+  },
+
+  // discord sets devtools callbacks over ipc
+  // this just disables the entire callback stuff to prevent needless errors
+  {
+    find: ".Messages.SELF_XSS_HEADER)",
+    replace: {
+      match: /null\!=\i&&"0\.0\.0"===\i\.app\.getVersion\(\)/,
+      replacement: "true"
+    }
   }
 ];
