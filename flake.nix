888d9ca017557d16952884b3a5fbbb5843367e8b
isabel <isabel@isabelroses.com>
2026-04-27 12:13:27 +0100

    nix: bump flake deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Yet another Discord mod";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
   };
 
   outputs =
