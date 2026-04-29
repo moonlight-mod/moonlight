4eda97d97f1aff238409e7b5ccd54de1dc28d72d
Cynthia Foxwell <gamers@riseup.net>
2026-04-28 20:21:33 -0600

    disableSentry: fix for asar with bundle

--- packages/core-extensions/src/disableSentry/host.ts
+++ packages/core-extensions/src/disableSentry/host.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { Module } from "node:module";
 import { join } from "node:path";
 
@@ -5,16 +6,52 @@ const logger = moonlightHost.getLogger("disableSentry");
 
 if (moonlightHost.asarPath !== "moonlightDesktop") {
   try {
-    const hostSentryPath = require.resolve(join(moonlightHost.asarPath, "node_modules", "@sentry", "electron"));
-    require.cache[hostSentryPath] = new Module(hostSentryPath, require.cache[require.resolve(moonlightHost.asarPath)]);
-    require.cache[hostSentryPath]!.exports = {
-      init: () => {},
-      captureException: () => {},
-      setTag: () => {},
-      setUser: () => {},
-      captureMessage: () => {}
-    };
-    logger.debug("Stubbed Sentry host side!");
+    if (existsSync(join(moonlightHost.asarPath, "node_modules", "@sentry", "electron"))) {
+      const hostSentryPath = require.resolve(join(moonlightHost.asarPath, "node_modules", "@sentry", "electron"));
+      require.cache[hostSentryPath] = new Module(
+        hostSentryPath,
+        require.cache[require.resolve(moonlightHost.asarPath)]
+      );
+      require.cache[hostSentryPath]!.exports = {
+        init: () => {},
+        captureException: () => {},
+        setTag: () => {},
+        setUser: () => {},
+        captureMessage: () => {}
+      };
+      logger.debug("Stubbed Sentry host side!");
+    } else if (existsSync(join(moonlightHost.asarPath, "bundle.js"))) {
+      let realGetGlobalSentry: any;
+      Object.defineProperty(Object.prototype, "getGlobalSentry", {
+        configurable: true,
+        set(getGlobalSentry) {
+          realGetGlobalSentry = getGlobalSentry;
+          if (this.init) {
+            const oldInit = this.init;
+            this.init = (buildInfo: any, _sentry: any) => {
+              oldInit(buildInfo);
+            };
+
+            Object.defineProperty(this, "getGlobalSentry", {
+              configurable: true,
+              writable: true,
+              enumerable: true,
+              value: getGlobalSentry
+            });
+
+            logger.debug("Patched crash reporter to not have Sentry");
+
+            // @ts-expect-error yes it in fact doesn't exist
+            delete Object.prototype.getGlobalSentry;
+          }
+        },
+        get() {
+          return realGetGlobalSentry;
+        }
+      });
+    } else {
+      logger.error("Cannot find Sentry, something in asar changed");
+    }
   } catch (err) {
     logger.error("Failed to stub Sentry host side:", err);
   }
--- packages/core-extensions/src/disableSentry/node.ts
+++ packages/core-extensions/src/disableSentry/node.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import Module from "node:module";
 import { resolve } from "node:path";
 import { constants } from "@moonlight-mod/types";
@@ -7,15 +8,48 @@ const logger = moonlightNode.getLogger("disableSentry");
 
 const preloadPath = ipcRenderer.sendSync(constants.ipcGetOldPreloadPath);
 try {
-  const sentryPath = require.resolve(resolve(preloadPath, "..", "node_modules", "@sentry", "electron"));
-  require.cache[sentryPath] = new Module(sentryPath, require.cache[require.resolve(preloadPath)]);
-  require.cache[sentryPath]!.exports = {
-    init: () => {},
-    setTag: () => {},
-    setUser: () => {},
-    captureMessage: () => {}
-  };
-  logger.debug("Stubbed Sentry node side!");
+  if (existsSync(resolve(preloadPath, "..", "node_modules", "@sentry", "electron"))) {
+    const sentryPath = require.resolve(resolve(preloadPath, "..", "node_modules", "@sentry", "electron"));
+    require.cache[sentryPath] = new Module(sentryPath, require.cache[require.resolve(preloadPath)]);
+    require.cache[sentryPath]!.exports = {
+      init: () => {},
+      setTag: () => {},
+      setUser: () => {},
+      captureMessage: () => {}
+    };
+    logger.debug("Stubbed Sentry node side!");
+  } else if (existsSync(resolve(preloadPath, "..", "bundle.js"))) {
+    let realGetGlobalSentry: any;
+    Object.defineProperty(Object.prototype, "getGlobalSentry", {
+      configurable: true,
+      set(getGlobalSentry) {
+        realGetGlobalSentry = getGlobalSentry;
+        if (this.init) {
+          const oldInit = this.init;
+          this.init = (buildInfo: any, _sentry: any) => {
+            oldInit(buildInfo);
+          };
+
+          Object.defineProperty(this, "getGlobalSentry", {
+            configurable: true,
+            writable: true,
+            enumerable: true,
+            value: getGlobalSentry
+          });
+
+          logger.debug("Patched crash reporter to not have Sentry");
+
+          // @ts-expect-error yes it in fact doesn't exist
+          delete Object.prototype.getGlobalSentry;
+        }
+      },
+      get() {
+        return realGetGlobalSentry;
+      }
+    });
+  } else {
+    logger.error("Cannot find Sentry, something in asar changed");
+  }
 } catch (err) {
   logger.error("Failed to stub Sentry:", err);
 }
