2db8e1ac40abf13298e81f093b40a460276b064c
Cynthia Foxwell <gamers@riseup.net>
2026-05-13 12:20:48 -0600

    mappings: fix TimestampTooltip find

--- packages/mappings/src/mappings/discord/modules/messages/web/TimestampTooltip.ts
+++ packages/mappings/src/mappings/discord/modules/messages/web/TimestampTooltip.ts
@@ -4,7 +4,7 @@ register((moonmap) => {
   const name = "discord/modules/messages/web/TimestampTooltip";
   moonmap.register({
     name,
-    find: ["\u2014", "timeFormatted:"],
+    find: ['" \\u2014 "', "timeFormatted:"],
     process({ id }) {
       moonmap.addModule(id, name);
 
