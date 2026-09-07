8871a8d2c25622fc1901346744449dc22a7b7582
Cynthia Foxwell <gamers@riseup.net>
2026-09-07 09:29:00 -0600

    markdown: fix slate patch

--- packages/core-extensions/src/markdown/index.ts
+++ packages/core-extensions/src/markdown/index.ts
@@ -15,12 +15,12 @@ export const patches: Patch[] = [
     find: "then you probably need to add it to this file so that the rich chat box understands it.",
     replace: [
       {
-        match: /(.)={link:{(.+?)},(.)=new Set/,
+        match: /(\i)={link:{(.+?)},(\i)=new Set/,
         replacement: (_, rulesDef, rules, syntaxBefore) =>
           `__slateRules,${rulesDef}=__slateRules=require("markdown_markdown")._addSlateRules({link:{${rules}}),${syntaxBefore}=new Set`
       },
       {
-        match: /(originalMatch:\i}=(\i);)(.+?)case"emoticon":(return .+?;)(.+?)case"subtext":{(.+?)}default:/,
+        match: /(:(\i)\.originalMatch;)(switch.+?)case"emoticon":(return .+?;)(.+?)case"subtext":{(.+?)}default:/,
         replacement: (_, start, rule, body, plaintextReturn, otherRules, inlineStyleBody) =>
           `${start}if(${rule}.type.startsWith("__moonlight_")){if(__slateRules[${rule}.type].type=="inlineStyle"){${inlineStyleBody}}else{${plaintextReturn}}}${body}case"emoticon":${plaintextReturn}${otherRules}case"subtext":{${inlineStyleBody}}default:`
       }
